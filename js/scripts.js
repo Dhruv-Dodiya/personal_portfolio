69fc76eb909b5058f55980eeb61c4cea24ffcea5
dhruv_dodiya <dhruv.d@gigglesinfotec.in>
2024-09-02 18:19:32 +0530

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// Sticky Navbar
+window.onscroll = function() {stickyNavbar()};
+
+var navbar = document.querySelector("nav");
+var sticky = navbar.offsetTop;
+
+function stickyNavbar() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// Smooth Scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
